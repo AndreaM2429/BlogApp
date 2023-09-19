5eadd63468fc1bb0d57999b009a0ba1af8fe9572
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-19 13:11:54 -0600

    Create model class for likes table

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  after_save :update_likes_counter
+  
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
+end
