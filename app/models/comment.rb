0fde955b6075b6eaa4b3b46f381c134838a715cc
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-19 13:11:30 -0600

    Create model class for comments table

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  belongs_to :post
+
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+end
