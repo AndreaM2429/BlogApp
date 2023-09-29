c4c4cbc37f1728fdf2eb0db272aba8a871cc0153
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-28 22:27:58 -0600

    Add new, create, createlike, deletelike actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,47 @@ class PostsController < ApplicationController
 
     @posts = @user.posts
   end
+  
+  def new
+    @user_current = current_user
+    @post_new = @user_current.posts.new
+  end
+
+  def create
+    post = current_user.posts.new(posts_params)
+    if post.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
+    @liked = like_exist?(@post, current_user)
+  end
+
+  def createlike
+    @post = Post.find(params[:id])
+    Like.create(post: @post, author: current_user)
+    redirect_to user_post_path(@post.author, @post)
+  end
+
+  def deletelike
+    @post = Post.find(params[:id])
+    @like = @post.likes.find_by(author: current_user)
+    @like.destroy
+    @like.update_likes_counter
+    redirect_to user_post_path(@post.author, @post)
+  end
+
+  private
+
+  def posts_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  def like_exist?(post, user_id)
+    post.likes.where(author: user_id)
   end
 end
