f8abd5e567952bef2c0c7e1741a9b69c741bb461
Andrea Manuel <andreamanuel2207@gmail.com>
2023-09-28 22:23:48 -0600

    Improve the routes using resources and add post, delete and comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  resources :users , only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
+      post 'createlike', on: :member
+      delete 'deletelike', on: :member
+      resources :comments, only: [:new, :create]
+    end
+  end
 end
