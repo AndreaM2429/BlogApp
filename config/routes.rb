fcf560646f77e442a844088bc6cd398cf576cba5
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-04 11:20:33 -0600

    Set user#index as root path and delete home files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
+  root 'users#index'
 
   resources :users , only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
