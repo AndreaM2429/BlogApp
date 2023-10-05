b5b12b7f3f8257ebf8c287793daa236bb4df5f0a
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-05 17:12:26 -0300

    Add 'role' for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo role])
   end
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+      user.permit(:name, :bio, :photo, :email, :password, :password_confirmation, :role)
     end
   end
 end
