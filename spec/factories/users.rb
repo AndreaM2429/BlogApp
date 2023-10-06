6d0946b7de81612b0ffb49e82fb009b8bf085956
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-06 10:38:21 -0600

    Change user factory to match with the new users table

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     photo { 'https://unsplash.com/photos/F_-0BxGuVvo' }
     bio { 'Teacher from Mexico.' }
     postsCounter { 0 }
+    email { 'example@example.com' }
+    password { '1234567890' }
+    confirmation_token { rand(1000..2000).to_s }
+    confirmed_at { Time.now }
+    confirmation_sent_at { Time.now }
+    role { 'Admin' }
   end
 end
